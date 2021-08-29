0f805653c3fb627999a230ad266716c9cf636e72
piumal1999 <piumalrathnayake@hotmail.com>
2021-08-29 22:03:12 +0530

    Print current time

--- clock.c
+++ clock.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 int main(int argc, char **argv){
     if(argc > 3) {
@@ -27,7 +28,9 @@ int main(int argc, char **argv){
     printf("\e[J"); // clear screen
 
     for(int i=0;i<300000;i++){
-        printf("This will be the time")
+        time_t currentTime;
+        time(&currentTime);
+        printf("%s", asctime(localtime(&currentTime)));
         printf("\e[H"); // move cursor to home position
     }
 
